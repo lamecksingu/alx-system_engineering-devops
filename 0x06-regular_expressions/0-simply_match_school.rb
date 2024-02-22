db13a800271c00abf6be2c42b12d759a16fba550
lamecksingu <singulameck9619@gmail.com>
2024-02-22 17:35:17 +0300

    A script to accept input to match a regex School

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#check if an argument is provided
+#
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <text>"
+    exit 1
+end
+#get the argument
+text = ARGV[0]
+pattern = /School/
+#use the regx to match the text
+match_result = text.match(pattern)
+#print result or an empty string if no match
+puts match_result ? match_result[0] : ''
