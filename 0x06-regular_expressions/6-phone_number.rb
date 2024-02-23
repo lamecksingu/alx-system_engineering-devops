bccf0f68f5c49423cc270f9df364420781bb5353
lamecksingu <singulameck9619@gmail.com>
2024-02-23 15:33:58 +0300

    Phone number

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <text>"
+    exit 1
+end
+#get the argument
+text = ARGV[0]
+
+#define the regex
+pattern = /\A\d{10}/
+
+#use regex to match the text
+match_result = text.match(pattern)
+
+#print the results
+puts match_result ? match_result[0] : ''
