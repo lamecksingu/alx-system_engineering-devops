86509d18bb21b23d0af1a808248899563f8f175c
lamecksingu <singulameck9619@gmail.com>
2024-08-02 02:15:46 +0300

    Automating configurations using puppette

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,25 +3,26 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Define custom HTTP header
-file { '/etc/nginx/sites-available/custom-header':
+# Define custom HTTP header configuration in the Nginx configuration file
+file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "server {
                 listen 80 default_server;
                 root /var/www/html;
                 index index.html;
                 location / {
-                    add_header X-Served-By $hostname;
+                    add_header X-Served-By \$hostname;
                     try_files \$uri \$uri/ =404;
                 }
             }",
+  notify  => Service['nginx'],  # Notify to restart Nginx if the file changes
 }
 
-# Create a symlink to enable the custom header configuration
-file { '/etc/nginx/sites-enabled/custom-header':
+# Create a symlink to enable the default configuration
+file { '/etc/nginx/sites-enabled/default':
   ensure => link,
-  target => '/etc/nginx/sites-available/custom-header',
-  require => File['/etc/nginx/sites-available/custom-header'],
+  target => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
 }
 
 # Ensure Nginx service is running
